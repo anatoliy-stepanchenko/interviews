f88e319f78ca1202ca98d1d670d2c4ba37cd3a92
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2024-06-26 18:02:26 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,27 @@ import 'primeflex/primeflex.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { initializeApp } from 'firebase/app'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+import 'primevue/resources/themes/saga-blue/theme.css'
 
 import Menubar from 'primevue/menubar'
 
 import App from './App.vue'
 import router from './router'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAfISk9PhGuhgJ8lWrOTo0_Ocglmtp8duw',
+  authDomain: 'interviews-ac397.firebaseapp.com',
+  projectId: 'interviews-ac397',
+  storageBucket: 'interviews-ac397.appspot.com',
+  messagingSenderId: '1085004829146',
+  appId: '1:1085004829146:web:72e34916da6dde29b7b0e3'
+}
+
+initializeApp(firebaseConfig)
 
 const app = createApp(App)
 
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,7 +1,50 @@
 <template>
-  <div>auth</div>
+  <!-- <app-toast position="bottom-right" /> -->
+  <div class="flex justify-content-center p-2">
+    <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+      <div class="text-center mb-3">
+        <div class="text-900 text-3xl font-medium mb-3">Вітаю!</div>
+        <span class="text-600 font-medium line-height-3">{{ subtitleText }}</span>
+        <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer" @click="toggleAuth">
+          {{ linkAccountText }}
+        </a>
+      </div>
+
+      <form @submit.prevent="submitForm">
+        <label for="email1" class="block text-900 font-medium mb-2">Email</label>
+        <app-input-text v-model="email" id="email1" type="email" class="w-full mb-3" />
+
+        <label for="password1" class="block text-900 font-medium mb-2">Пароль</label>
+        <app-input-text v-model="password" id="password1" type="password" class="w-full mb-3" />
+
+        <app-button
+          :label="submitButtonText"
+          type="submit"
+          icon="pi pi-user"
+          :loading="isLoading"
+          class="w-full"
+        ></app-button>
+      </form>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const isLogin = ref<boolean>(true)
+
+const subtitleText = computed(() => {
+  return isLogin.value ? 'Акаунта ще не має?' : 'Акаунт вже існує?'
+})
+
+const linkAccountText = computed(() => {
+  return isLogin.value ? 'Створіть зараз' : 'Ввійдіть в нього'
+})
+
+const toggleAuth = () => {
+  isLogin.value = !isLogin.value
+}
+</script>
 
 <style scoped></style>
