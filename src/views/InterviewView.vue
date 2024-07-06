8d379e20a12d4ccd93a2b1727e7446be8c80f520
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2024-07-06 15:36:32 +0300

    bug fixing

--- src/views/InterviewView.vue
+++ src/views/InterviewView.vue
@@ -1,7 +1,6 @@
 <template>
   <app-progress-spinner v-if="isLoading" class="flex justify-content-center mt-8" />
   <div v-else-if="interview?.id && !isLoading" class="content-interview">
-    {{ interview }}
     <app-card>
       <template #title>Співбесіда в компанію {{ interview.company }}</template>
       <template #content>
@@ -95,7 +94,6 @@
               <label :for="`stage-date-${index}`">Дата проходження етапу</label>
               <app-calendar
                 v-model="stage.date"
-                @date-select="saveDateStage(index)"
                 class="input mb-3"
                 :id="`stage-date-${index}`"
                 dateFormat="dd.mm.yy"
@@ -146,11 +144,10 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { getFirestore, doc, getDoc, updateDoc } from 'firebase/firestore'
+import { getFirestore, doc, getDoc, updateDoc, Timestamp } from 'firebase/firestore'
 import { useUserStore } from '@/stores/user'
 import { useRoute } from 'vue-router'
-import type { IInterview } from '@/interfaces'
-import dayjs from 'dayjs'
+import type { IInterview, IStage } from '@/interfaces'
 
 const db = getFirestore()
 const userStore = useUserStore()
@@ -164,7 +161,25 @@ const docref = doc(db, `users/${userStore.userId}/interviews`, route.params.id a
 const getData = async (): Promise<void> => {
   isLoading.value = true
   const docSnap = await getDoc(docref)
-  interview.value = docSnap.data() as IInterview
+
+  if (docSnap.exists()) {
+    const data = docSnap.data() as IInterview
+
+    if (data.stages && data.stages.length) {
+      data.stages = data.stages.map((stage: IStage) => {
+        if (stage.date && stage.date instanceof Timestamp) {
+          return {
+            ...stage,
+            date: stage.date.toDate()
+          }
+        }
+        return stage
+      })
+    }
+
+    interview.value = data
+  }
+
   isLoading.value = false
 }
 
@@ -173,7 +188,7 @@ const addStage = () => {
     if (!interview.value.stages) {
       interview.value.stages = []
     }
-    interview.value.stages.push({ name: '', date: '', description: '' })
+    interview.value.stages.push({ name: '', date: null, description: '' })
   }
 }
 
@@ -190,13 +205,6 @@ const saveInterview = async (): Promise<void> => {
   await updateDoc(docref, { ...interview.value })
   await getData()
   isLoading.value = false
-}
-
-const saveDateStage = (index: number) => {
-  if (interview.value?.stages && interview.value.stages.length) {
-    const date = interview.value.stages[index].date
-    interview.value.stages[index].date = dayjs(date).format('DD.MM.YYYY')
-  }
 }
 
 onMounted(async () => await getData())
